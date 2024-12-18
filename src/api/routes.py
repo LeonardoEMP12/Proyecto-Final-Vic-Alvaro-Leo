e330cd416565b1f76ce4973f5d56bbc8dc31e464
Alvaro <alvaro.albalad@gmail.com>
2024-12-18 19:56:39 +0000

    contraseñas codificadas

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,11 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_bcrypt import Bcrypt 
+
 
 api = Blueprint('api', __name__)
+bcrypt = Bcrypt() 
 
 # Allow CORS requests to this API
 CORS(api)
@@ -20,3 +23,31 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+
+@api.route('/signup', methods=['POST'])
+def handle_register():
+    request_body = request.json # Recogemos los datos del body mandado  
+    name = request_body.get('name') # Recogemos el campo name del request_body
+    email = request_body.get('email') # Recogemos el campo email del request_body
+    password = request_body.get('password') # Recogemos el campo password del request_body
+    creation_date = request_body.get('creation_date') # Recogemos el campo creation_date del request_body
+
+    if not name or not password or not email or not creation_date: # Validamos si existen los campos email, username y password
+        return jsonify({"message": "Todos los campos deben estar completos"}), 400
+    
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    
+    user = User.query.filter_by(name = name, email = email).first() # Buscamos dentro de la tabla user si hay ya un usuario que contenga los mismos datos
+
+    if user: # Validamos si existe ese usuario dentro de la base de datos
+        return jsonify({"message": "Ese usuario ya existe"}), 400
+    
+    usuario_add = User(name = name, email = email, password = hashed_password, creation_date = creation_date) # Indicamos la inserccion que deseamos hacer en la base de datos
+
+    db.session.add(usuario_add) # Realizamos la insercion
+    db.session.commit() # Actualizamos la base de datos
+
+     # Retornamos los datos añadidos
+    usuario_add_serialize = usuario_add.serialize()
+    return jsonify({"response":hashed_password}), 200
