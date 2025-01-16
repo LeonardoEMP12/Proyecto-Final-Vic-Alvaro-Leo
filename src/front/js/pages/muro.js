2146b2ba16acb259da9486a645349c75e8207507
Leonardo Medina <leonardomedina1250@gmail.com>
2025-01-16 10:17:49 +0000

    Boton de post nuevo

--- src/front/js/pages/muro.js
+++ src/front/js/pages/muro.js
@@ -29,8 +29,59 @@ const Muro = () => {
 
             <div className="col-md-8 fondo2">
                 <div className="d-flex justify-content-end mt-3">
-                    <div className="me-3">Añadir publicacion</div>
+                <button
+                        id="botonPost"
+                        type="button"
+                        className="btn btn-primary me-3"
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal"
+                    >
+                        Añadir publicación
+                    </button>
                     <div>Editar Perfil</div>
+                    <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                >
+                    <div className="modal-dialog">
+                        <div id="PostModal" className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalLabel">Nueva publicación</h5>
+                                <button
+                                    type="button"
+                                    className="btn-close"
+                                    data-bs-dismiss="modal"
+                                    aria-label="Close"
+                                ></button>
+                            </div>
+                            <div className="modal-body">
+                                <form>
+                                    <div className="mb-3">
+                                        <label htmlFor="message-text" className="col-form-label">
+                                        ¿Listo para jugar?:
+                                        </label>
+                                        <textarea className="form-control" id="message-text"></textarea>
+                                    </div>
+                                </form>
+                            </div>
+                            <div className="modal-footer">
+                                <button
+                                    type="button"
+                                    className="btn btn-secondary"
+                                    data-bs-dismiss="modal"
+                                >
+                                    Cerrar
+                                </button>
+                                <button type="button" className="btn btn-primary">
+                                    Publicar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 </div>
                 {post.map((post) => (
                     <div className="cuerpo">
