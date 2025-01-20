8622de073c933f781d95488674dfd0a4a7840893
Leonardo Medina <leonardomedina1250@gmail.com>
2025-01-20 18:34:15 +0000

    Landing corregida

--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../styles/landingpage.css";
 import OMNIAlogo from "../../img/LogoOM.png";
-import landingomnia from "../../img/landingomnia.png";
 import { Link } from "react-router-dom";
 
 const Landing = () => {
@@ -12,13 +11,16 @@ const Landing = () => {
 					<div id="ContLanding" className="col-10 col-md-8 col-lg-6 d-flex flex-column justify-content-center align-items-center formulario-contenedor mx-auto mt-5">
 					<Link to="/login"><button id="Inicio" type="button" className="btn btn-secondary">Iniciar sesion</button></Link>
 					<Link to="/register"><button id="Registro" type="button" className="btn btn-secondary mt-4">Registrarse</button></Link>
-						<div className="text-center mt-5">
+						{/* <div className="text-center mt-5">
 							<img src={OMNIAlogo} alt="OMNIA Logo" className="img-fluid" />
-						</div>
+						</div> */}
+					</div>
+					<div>
+					<h1 id="Frase" className="mt-5" >Tu red Social para videojuegos</h1>
 					</div>
 				</div>
-				<div className="col-6 d-none d-md-flex d-flex justify-content-center align-items-center bg-succes">
-					<img src={landingomnia} alt="OMNIA Landing" className="img-fluid" />
+				<div className="col-6 d-none d-md-flex d-flex justify-content-center align-items-center bg-succes logo-container">
+					<img src={OMNIAlogo} alt="OMNIA Logo" className="img-fluid" />
 				</div>
 			</div>
 		</div>
