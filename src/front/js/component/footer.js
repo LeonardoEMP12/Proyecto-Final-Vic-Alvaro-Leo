f14e650ec994b09cb1ac9fa70b97baab25cd4452
Vichum <100027914+ElVichum@users.noreply.github.com>
2025-01-20 17:25:27 +0000

    hecho footer mas pequeño

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer pt-2 text-center">
 		<p>
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>{" "}
 			<a href="#">Contáctanos</a>{" "}
