6407f853b77ea97a6f0b847c5e9efc00ab85921e
Leonardo Medina <leonardomedina1250@gmail.com>
2025-01-22 13:39:29 +0000

    estirar imagenes Noticias

--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -11,11 +11,11 @@ const Landing = () => {
 					<div id="ContLanding" className="col-10 col-md-8 col-lg-6 d-flex flex-column justify-content-center align-items-center formulario-contenedor mx-auto mt-5">
 					<Link to="/login"><button id="Inicio" type="button" className="btn btn-secondary">Iniciar sesion</button></Link>
 					<Link to="/register"><button id="Registro" type="button" className="btn btn-secondary mt-4">Registrarse</button></Link>
-						{/* <div className="text-center mt-5">
+					<div className="text-center mt-5 d-block d-sm-none">
 							<img src={OMNIAlogo} alt="OMNIA Logo" className="img-fluid" />
-						</div> */}
+						</div>
 					</div>
-					<div>
+					<div className="text-center">
 					<h1 id="Frase" className="mt-5" >Tu red Social para videojuegos</h1>
 					</div>
 				</div>
